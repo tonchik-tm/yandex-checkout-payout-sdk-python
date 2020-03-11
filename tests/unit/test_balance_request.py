4cd1d285e75bd023d64e4140dc1c955672c7b816
Антон Н. Николаев <nikolaevanym@yamoney.ru>
2020-03-11 09:43:24 +0300

    CMSSDK-1265 - Пробные тесты

--- tests/unit/test_balance_request.py
+++ tests/unit/test_balance_request.py
@@ -7,27 +7,31 @@ from yandex_checkout_payout.domain.request.balance_request import BalanceRequest
 
 
 class TestBalanceRequest(unittest.TestCase):
+
     def test_request_cast(self):
         request = BalanceRequest()
         request.agent_id = 123456
         request.client_order_id = '215d8da0-000f-50be-b000-0003308c89be'
-        request.request_dt = '2020-03-04T15:39:45.456Z'
+        request.request_dt = '2020-03-04T15:39:45.456+03:00'
 
         self.assertEqual({
             'agent_id': '123456',
             'client_order_id': '215d8da0-000f-50be-b000-0003308c89be',
-            'request_dt': datetime.datetime(2020, 3, 4, 15, 39, 45, 456000)
+            'request_dt': datetime.datetime(2020, 3, 4, 15, 39, 45, 456000,
+                                            tzinfo=datetime.timezone(datetime.timedelta(seconds=10800))),
+            'request_name': 'balanceRequest'
         }, dict(request))
 
     def test_request_setters(self):
         request = BalanceRequest({
             'agent_id': 123456,
             'client_order_id': '215d8da0-000f-50be-b000-0003308c89be',
-            'request_dt': '2020-03-04T15:39:45.456Z'
+            'request_dt': '2020-03-04T15:39:45.456+03:00'
         })
 
         self.assertIsInstance(request.agent_id, str)
         self.assertIsInstance(request.request_dt, datetime.datetime)
+        self.assertEqual(request.request_name, 'balanceRequest')
 
         with self.assertRaises(TypeError):
             request.request_dt = 'invalid request_dt'
@@ -38,74 +42,3 @@ class TestBalanceRequest(unittest.TestCase):
         # with self.assertRaises(TypeError):
         #     request.settlements = 'invalid settlements'
 
-    def test_request_validate(self):
-        request = ReceiptRequest()
-
-        with self.assertRaises(ValueError):
-            request.validate()
-
-        request.type = ReceiptType.PAYMENT
-
-        with self.assertRaises(ValueError):
-            request.validate()
-
-        request.send = True
-
-        with self.assertRaises(ValueError):
-            request.validate()
-
-        request.customer = ReceiptCustomer({'phone': '79990000000', 'email': 'test@email.com'})
-
-        with self.assertRaises(ValueError):
-            request.validate()
-
-        request.items = [
-            ReceiptItem({
-                "description": "Product 1",
-                "quantity": 2.0,
-                "amount": {
-                    "value": 250.0,
-                    "currency": Currency.RUB
-                },
-                "vat_code": 2
-            }),
-            ReceiptItem({
-                "description": "Product 2",
-                "quantity": 1.0,
-                "amount": {
-                    "value": 100.0,
-                    "currency": Currency.RUB
-                },
-                "vat_code": 2
-            })
-        ]
-
-        with self.assertRaises(ValueError):
-            request.validate()
-
-        request.settlements = [
-            Settlement({
-                'type': SettlementType.CASHLESS,
-                'amount': {
-                    'value': 250.0,
-                    'currency': Currency.RUB
-                }
-            })
-        ]
-
-        with self.assertRaises(ValueError):
-            request.validate()
-
-        request.tax_system_code = 1
-
-        with self.assertRaises(ValueError):
-            request.validate()
-
-        request.refund_id = '215d8da0-000f-50be-b000-0003308c89be'
-
-        with self.assertRaises(ValueError):
-            request.validate()
-
-        request.payment_id = '215d8da0-000f-50be-b000-0003308c89be'
-
-        self.assertIsNone(request.validate())
